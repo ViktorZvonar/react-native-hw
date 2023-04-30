7fa24651efc2817d2f52b608e4f06431d1ba108c
ViktorZvonar <viktorzvonar@yahoo.com>
2023-04-30 13:15:52 +0300

    Update PostsScreen.js

--- Screens/PostsScreen.js
+++ Screens/PostsScreen.js
@@ -1,6 +1,5 @@
-import { Text, View, TouchableOpacity } from "react-native";
-
 import React from "react";
+import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function PostsScreen({ navigation }) {
@@ -20,8 +19,43 @@ export default function PostsScreen({ navigation }) {
   }, [navigation]);
 
   return (
-    <View>
-      <Text>Posts Screen</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../images/Rectangle22.jpg")}
+        style={styles.userPhoto}
+      />
+      <Text style={[styles.textContainer, { flex: 1 }]}>
+        <Text style={styles.userName}>Natali Romanova</Text>
+        {"\n"}
+        <Text style={styles.userEmail}>email@example.com</Text>
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 16,
+    marginTop: 32,
+  },
+
+  textContainer: {
+    marginLeft: 8,
+  },
+
+  userPhoto: {
+    width: 60,
+    height: 60,
+    borderRadius: 16,
+  },
+  userName: {
+    fontSize: 13,
+    fontWeight: "bald",
+  },
+  userEmail: {
+    fontWeight: "normal",
+    fontSize: 11,
+  },
+});
